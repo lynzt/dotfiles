13a158e2b870c19dbb3ec5b050607e673e242230
lindsay t <techells22@gmail.com>
2019-08-07 00:16:14 -0500

    add doom config

---
+++ doom/.doom.d/.doom.d
@@ -0,0 +1,1 @@
+/Users/lynzt/.doom.d
---
+++ doom/.doom.d/config.el
@@ -0,0 +1,5 @@
+;;; ~/.doom.d/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here
+(setq
+ projectile-project-search-path '("~/coding/" "~/coding/tw/" "~/coding/twstuff"))
