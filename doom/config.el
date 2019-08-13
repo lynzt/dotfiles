79b13bd2b644c1e090b6c53e0d48fc8e81b5342c
lindsay t <techells22@gmail.com>
2019-08-12 21:58:40 -0500

    add goto prev buffer in doom

--- doom/config.el
+++ doom/config.el
@@ -1,5 +1,21 @@
 ;;; .config/doom/config.el -*- lexical-binding: t; -*-
 
 ;; Place your private configuration here
+;;
+;; relative line numbering
 (setq
-  projectile-project-search-path '("~/coding/" "~/coding/tw/"))
+  display-line-numbers-type 'relative)
+;;
+;; projectile - auto set search path
+(setq
+  projectile-project-search-path '("~/coding/" "~/coding/tw/" "~/coding/react"))
+
+;;; "Switch to previously open buffer.
+;;;     Repeated invocations toggle between the two most recently open buffers."
+(defun er-switch-to-previous-buffer ()
+  (interactive)
+  (switch-to-buffer (other-buffer (current-buffer) 1)))
+
+;;; er-switch-to-previous-buffer
+(map! (:when
+        :n "C-c b" #'er-switch-to-previous-buffer))
