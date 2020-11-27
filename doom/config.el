28dca36391a866fdbb18762a0a283c19595655e3
lindsay t <techells22@gmail.com>
2020-11-26 21:47:34 -0600

    config changes

--- doom/config.el
+++ doom/config.el
@@ -10,10 +10,6 @@
 (setq
   projectile-project-search-path '("~/coding/" "~/coding/tw/" "~/coding/react" "~/coding/interviews"))
 
-(after! org
-  (setq org-agenda-files (list "~/Dropbox/emacs/todo.org" )))
-
-
 ;;; "Switch to previously open buffer.
 ;;;     Repeated invocations toggle between the two most recently open buffers."
 (defun er-switch-to-previous-buffer ()
@@ -24,8 +20,8 @@
 (map! (:when
         :n "C-c b" #'er-switch-to-previous-buffer))
 
-(setq
- treemacs-position 'right)
+;;; (setq
+;;; treemacs-position 'right)
 
 (global-set-key "\C-x2" (lambda () (interactive)(split-window-vertically) (other-window 1)))
 (global-set-key "\C-x3" (lambda () (interactive)(split-window-horizontally) (other-window 1)))
@@ -34,9 +30,13 @@
       evil-insert-state-cursor '(bar "medium sea green")
       evil-visual-state-cursor '(hollow "orange"))
 
-;;; (setq doom-theme 'doom-molokai)
+(setq doom-theme 'doom-dark+)
+;; (setq doom-theme 'doom-losvkem)
+;; (setq doom-theme 'doom-molokai)
 ;;; (setq doom-theme 'doom-nord)
-(setq doom-theme 'doom-dracula)
+;;; (setq doom-theme 'doom-dracula)
+
+(setq doom-font (font-spec :family "Inconsolata" :size 14))
 
 ;;; modeline stuff - https://github.com/seagle0128/doom-modeline
 (setq doom-modeline-icon (display-graphic-p))
@@ -45,3 +45,15 @@
 (setq doom-modeline-indent-info t)
 (setq doom-modeline-buffer-file-name-style 'truncate-upto-root)
 (setq doom-modeline-modal-icon t)
+
+
+(after! org
+  (setq org-agenda-files (list "~/Dropbox/emacs/todo.org" )))
+
+;; roam settings
+(after! org
+  (setq org-directory "~/Dropbox/emacs/org/"
+        org-roam-directory (concat org-directory "roam")))
+
+;; show invisibles
+(global-whitespace-mode +1)
